9348c49e9cdf955c8b44e509f59c3dfc7f51997d
Eric Seidel <gridaphobe@gmail.com>
2012-10-29 14:03:17 -0700

    get rid of gnome for now

---
+++ xbindkeysrc.scm
@@ -0,0 +1,7 @@
+;;; Media keys
+
+;; (xbindkey '(XF86AudioRaiseVolume) "amixer set Master 5%+")
+;; (xbindkey '(XF86AudioLowerVolume) "amixer set Master 5%+")
+;; (xbindkey '(XF86AudioMute) "amixer set Master toggle")
+(xbindkey '(XF86MonBrightnessUp) "light 10+")
+(xbindkey '(XF86MonBrightnessDown) "light 10-")
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,16 +4,25 @@ import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.EwmhDesktops
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Util.EZConfig
+import           XMonad.Util.Run
 
-main = xmonad =<< xmobar myConfig
+import           System.IO
 
-myConfig = ewmh desktopConfig
-    { modMask = mod4Mask
-    , terminal = "urxvtc -e fish"
-    , manageHook = myManageHook <+> manageHook desktopConfig
-    , layoutHook = avoidStruts  $  layoutHook desktopConfig
-    , workspaces = ["1:web", "2:dev", "3:mail", "4:irc"] ++ map show [5..9]
-    }
+
+main = do
+    xmproc <- spawnPipe "xmobar -x 0"
+    -- xmproc' <- spawnPipe "xmobar -x 1"
+    xmonad $ ewmh desktopConfig
+        { modMask = mod4Mask
+        , terminal = "urxvtc -e fish"
+        , manageHook = myManageHook <+> manageHook desktopConfig
+        , layoutHook = avoidStruts  $  layoutHook desktopConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                    { ppOutput = \o -> hPutStrLn xmproc o -- >> hPutStrLn xmproc' o
+                    , ppTitle = xmobarColor "green" "" . shorten 50
+                    }
+        , workspaces = ["1:web", "2:dev", "3:mail", "4:irc"] ++ map show [5..9]
+        }
 
 myManageHook = composeAll
     [ className =? "gnus" --> doShift "3:mail"
